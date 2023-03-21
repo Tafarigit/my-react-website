6e3131fdaa3e26e9e337d2870cbfac6949c39216
Tafari Excell <excell.tafari@gmail.com>
2023-03-21 11:07:13 -0400

    Links added for Home and About and Users

--- Documents/pursuit-folder/9-3-module-3/react-repos/my-react-website/my-react-website/src/App.js
+++ Documents/pursuit-folder/9-3-module-3/react-repos/my-react-website/my-react-website/src/App.js
@@ -1,14 +1,30 @@
-import logo from './logo.svg';
-import Nav from "./Components/Nav"
-import './App.css';
+import logo from "./logo.svg";
+import Nav from "./Components/Nav";
+import "./App.css";
+import { Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
 
 function App() {
   return (
-    <div className="App">
-      <header>App
-        <Nav />
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header>
+          App
+          <Nav />
+          <Home />
+          <About />
+          <Routes>
+            <Route path="/">
+              <Route path="/about">
+                <Route path="users"></Route>
+              </Route>
+            </Route>
+          </Routes>
+        </header>
+      </div>
+    </Router>
   );
 }
 
---
+++ Documents/pursuit-folder/9-3-module-3/react-repos/my-react-website/my-react-website/src/Home.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h2>This is the Home Page!</h2>
+    </div>
+  );
+}
